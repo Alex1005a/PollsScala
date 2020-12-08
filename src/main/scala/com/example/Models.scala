ec53301593c7b2f7ed234cf5cb95915a9b17f4e2
Rosia <azazf932gmail.com>
2020-12-08 21:20:39 +0300

    Add cases classes for mongodb

--- src/main/scala/com/example/Models.scala
+++ src/main/scala/com/example/Models.scala
@@ -6,12 +6,13 @@ import org.mongodb.scala.bson.ObjectId
 
 object Models {
   final case class Vote(answer: String, count: Int = 0)
-  final case class Poll(_id: String, question: String, votes: List[Vote], dateBegin: Option[LocalDateTime], timeInMinutes: Int, authorId: String)
-  final case class Answer(_id: String, pollId: String, answerNumber: Int, userId: String)
+  final case class Poll(id: String, question: String, votes: List[Vote], dateBegin: Option[LocalDateTime], timeInMinutes: Int, authorId: String)
+  final case class Answer(id: String, pollId: String, answerNumber: Int, userId: String)
 
   object Poll {
-    def apply(pollDto: PollDto, userId: String): Poll =
+    def apply(pollDto: PollDto, userId: String): Poll = {
       Poll(new ObjectId().toHexString, pollDto.question, pollDto.answers.map(x => Vote(x)), None, pollDto.timeInMinutes, userId)
+    }
   }
 
   object Answer {
--- src/main/scala/com/example/Repository.scala
+++ src/main/scala/com/example/Repository.scala
@@ -1,9 +1,12 @@
 package com.example
 
+import java.time.LocalDateTime
+
 import com.example.Models.{Answer, Poll, Vote}
 import org.mongodb.scala.bson.codecs.Macros._
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.mongodb.scala.bson.ObjectId
 import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
 
@@ -11,42 +14,61 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.util.{Either, Right}
 
+private final case class PollMongo(_id: ObjectId, question: String, votes: List[Vote], dateBegin: Option[LocalDateTime], timeInMinutes: Int, authorId: String)
+private object PollMongo {
+  def apply(poll: Poll): PollMongo = {
+    PollMongo(new ObjectId(), poll.question, poll.votes, None, poll.timeInMinutes, poll.authorId)
+  }
+}
+private final case class AnswerMongo(_id: ObjectId, pollId: String, answerNumber: Int, userId: String)
+private object AnswerMongo {
+  def apply(answer: Answer): AnswerMongo = {
+    AnswerMongo(new ObjectId(), answer.pollId, answer.answerNumber, answer.userId)
+  }
+}
 
 object PollRepository {
-  private def getCollection: MongoCollection[Poll] = {
-    val codeRegistry = fromRegistries(fromProviders(classOf[Poll], classOf[Vote]), DEFAULT_CODEC_REGISTRY)
+  private def getCollection: MongoCollection[PollMongo] = {
+    val codeRegistry = fromRegistries(fromProviders(classOf[PollMongo], classOf[Vote]), DEFAULT_CODEC_REGISTRY)
     val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
     val database: MongoDatabase = mongoClient.getDatabase("scalaPoll").withCodecRegistry(codeRegistry)
     database.getCollection("polls")
   }
 
   def create(poll: Poll): Either[String, String] = {
-    Await.result(getCollection.insertOne(poll).toFuture, Duration.Inf)
-    Right(poll._id)
+    val p = PollMongo(poll)
+    Await.result(getCollection.insertOne(p).toFuture, Duration.Inf)
+    Right(p._id.toHexString)
   }
 
   def getById(id: String): Option[Poll] = {
-    val res = Await.result(getCollection.find(equal("_id", id)).toFuture, Duration.Inf)
-    res.headOption
+    val res = Await.result(getCollection.find(equal("_id", new ObjectId(id))).toFuture, Duration.Inf)
+    res.headOption.map(p => Poll(p._id.toHexString,
+                                 p.question,
+                                 p.votes,
+                                 p.dateBegin,
+                                 p.timeInMinutes,
+                                 p.authorId))
   }
 
   def update(poll: Poll): Either[String, _] = {
-    val res = Await.result(getCollection.replaceOne(equal("_id", poll._id), poll).toFuture, Duration.Inf)
+    val res = Await.result(getCollection.replaceOne(equal("_id", new ObjectId(poll.id)), PollMongo(poll)).toFuture, Duration.Inf)
     if(res.wasAcknowledged()) Right()
     else Left(res.toString)
   }
 }
 
 object AnswerRepository {
-  private def getCollection: MongoCollection[Answer] = {
-    val codeRegistry = fromRegistries(fromProviders(classOf[Answer]), DEFAULT_CODEC_REGISTRY)
+  private def getCollection: MongoCollection[AnswerMongo] = {
+    val codeRegistry = fromRegistries(fromProviders(classOf[AnswerMongo]), DEFAULT_CODEC_REGISTRY)
     val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
     val database: MongoDatabase = mongoClient.getDatabase("scalaPoll").withCodecRegistry(codeRegistry)
     database.getCollection("answers")
   }
 
   def create(answer: Answer): Either[String, String] = {
-    Await.result(getCollection.insertOne(answer).toFuture, Duration.Inf)
-    Right(answer._id)
+    val a = AnswerMongo(answer)
+    Await.result(getCollection.insertOne(a).toFuture, Duration.Inf)
+    Right(a._id.toHexString)
   }
 }
